0e6e11fcd77bc6aa5d4b409c69b27156abd270bb
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-19 18:34:30 +0900

    Listed packages

--- sampleapp.pp
+++ sampleapp.pp
@@ -1,1 +1,40 @@
-notice("Hello, World!")
+$packages = [
+  'autoconf',
+  'automake',
+  'binutils',
+  'bison',
+  'flex',
+  'gcc',
+  'gcc-c++',
+  'gettext',
+  'libtool',
+  'make',
+  'patch',
+  'pkgconfig',
+  'redhat-rpm-config',
+  'rpm-build',
+  'byacc',
+  'cscope',
+  'ctags',
+  'cvs',
+  'diffstat',
+  'doxygen',
+  'elfutils',
+  'gcc-gfortran',
+  'git',
+  'indent',
+  'intltool',
+  'patchutils',
+  'rcs',
+  'subversion',
+  'swig',
+  'systemtap',
+  'cmake',
+  'git',
+  'zsh'
+]
+
+package { $packages:
+  ensure => installed
+}
+
