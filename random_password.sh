d947522f83e5b76d8a4a156ff3877fb32f48b615
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-21 14:12:42 +0900

    Bug fix

--- random_password.sh
+++ random_password.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-prefix="$mysql_password = \""
-passwd=`date +%s | base64 | head -c 16`
+prefix="\$mysql_password = \""
+passwd=$(date +%s | sha1sum | base64 | head -c 16)
 suffix="\""
 
 str=$prefix$passwd$suffix
