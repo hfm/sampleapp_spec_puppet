939d51bbc9ff7a12946143decba5e79a2db2835a
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-21 18:07:02 +0900

    Bug fix for directory presence

--- puppet.d/modules/rbenv/manifests/rubybuild.pp
+++ puppet.d/modules/rbenv/manifests/rubybuild.pp
@@ -1,14 +1,13 @@
 class rbenv::rubybuild {
-  $plugin_path = '/usr/local/rbenv/plugins/'
-  file { '$plugin_path':
-    present => true,
+  file { '/usr/local/rbenv/plugins':
+    ensure => present,
     owner   => 'root',
     group   => 'appuser'
   }
 
   $install_path = '/usr/local/rbenv/plugins/ruby-build'
   exec { 'rbenv::install':
-    command => "git clone https://github.com/sstephenson/ruby-build.git $install_path",
+    command => 'git clone https://github.com/sstephenson/ruby-build.git $install_path',
     owner   => 'root',
     group   => 'appuser',
     creates => $install_path,
