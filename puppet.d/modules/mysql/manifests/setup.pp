7dba9436ec6893a64e6069074dd65d2e711cb525
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-21 15:54:41 +0900

    Removed scripts about createdb and grant

--- puppet.d/modules/mysql/manifests/setup.pp
+++ puppet.d/modules/mysql/manifests/setup.pp
@@ -5,22 +5,4 @@ class mysql::setup {
     command => 'mysqladmin -uroot password $mysql_password',
     require => Service['mysqld']
   }
-
-  $user = 'okkun'
-  $password = 'hogehoge'
-  $dbname = 'sampleapp'
-
-  exec { "create-${dbname}-db":
-    unless  => "mysql -uroot ${dbname}",
-    command => "mysql -uroot -e \"create database ${dbname};\"",
-    require => Service['mysqld']
-  }
-
-  exec { "grant-${dbname}-db":
-    unless  => "mysql -u${user} -p${password} ${dbname}",
-    command => "mysql -uroot -e
-      \"GRANT ALL ON ${dbname}.* TO ${user}@localhost
-      IDENTIFIED BY '${password}';\"",
-    require => [Service['mysqld'], Exec["create-${dbname}-db"]]
-  }
 }
