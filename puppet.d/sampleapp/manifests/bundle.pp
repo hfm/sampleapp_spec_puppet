aff8a9fded9c57d2a7ce1335829e2d8640d6b559
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-21 17:37:19 +0900

    Fixed directory path in install manifest

--- puppet.d/sampleapp/manifests/bundle.pp
+++ puppet.d/sampleapp/manifests/bundle.pp
@@ -1,10 +1,11 @@
-class sampleapp::checkout {
+class sampleapp::bundle {
   $app_path = '/var/www/sample_app'
-  exec { 'sampleapp::install':
-    command => "git clone https://github.com/Tacahilo/sample_app.git ${app_path}",
+  exec { 'sampleapp::bundle':
+    command => "bundle install",
     user    => 'root',
     group   => 'appuser',
     creates => $app_path,
+    cwd     => $app_path,
     path    => ['/bin', '/usr/bin'],
     timeout => 100
   }
