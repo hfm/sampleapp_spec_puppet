7d97d171f5d60fec60b6577f0d239ebf9c8a1c33
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-21 17:16:53 +0900

    Fixed install_path

--- puppet.d/sampleapp/manifests/checkout.pp
+++ puppet.d/sampleapp/manifests/checkout.pp
@@ -1,7 +1,7 @@
 class sampleapp::checkout {
   $app_path = '/var/www/'
   exec { 'rbenv::install':
-    command => "git clone https://github.com/Tacahilo/sample_app.git ${app_path}",
+    command => "git clone https://github.com/Tacahilo/sample_app.git ${app_path}/sample_app",
     user    => 'root',
     group   => 'appuser',
     creates => $app_path,
