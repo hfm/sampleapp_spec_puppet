9c71f7a2462393f5497558a571e96fdf90e00d59
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-01 16:11:53 +0900

    Create db and user

--- puppet.d/modules/mysql/manifests/setup.pp
+++ puppet.d/modules/mysql/manifests/setup.pp
@@ -1,8 +1,24 @@
 class mysql::setup {
+  # 実際これは良くないけど、今回だけ
+  $user = 'okkun'
+  $name = 'app'
+  $password = 'hogehoge'
+  $mysql_password = 'fugafuga'
+
   exec { 'set-mysql-password':
-    unless  => 'mysqladmin -uroot -p$mysql_password status',
+    unless  => "mysqladmin -uroot -p${mysql_password} status",
     path    => ['/bin', '/usr/bin'],
-    command => 'mysqladmin -uroot password $mysql_password',
+    command => "mysqladmin -uroot password ${mysql_password}",
     require => Service['mysqld']
+  }
+
+  exec { "create-${name}-db":
+    unless  => "mysql -u${user} -p${password} ${name}",
+    command => "mysql -uroot -p${mysql_password} -e
+                \"CREATE DATABASE ${name};
+                GRANT ALL ON ${name}.* TO ${user}@localhost
+                IDENTIFIED BY '${password}';\"",
+    require => Service['mysqld'],
+    require => Exec['set-mysql-password']
   }
 }
