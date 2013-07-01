31eb3b9894331c3b7f1e20067ff6dbc222d387f6
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-01 11:16:35 +0900

    add ruby2.0.0-p247

--- puppet.d/modules/rbenv/manifests/init.pp
+++ puppet.d/modules/rbenv/manifests/init.pp
@@ -2,10 +2,10 @@ class rbenv {
   include rbenv::install
   include rbenv::config
   include rbenv::rubybuild
-  include rbenv::ruby200p195
+  include rbenv::ruby200p247
 
   Class['rbenv::install']
   -> Class['rbenv::config']
   ~> Class['rbenv::rubybuild']
-  ~> Class['rbenv::ruby200p195']
+  ~> Class['rbenv::ruby200p247']
 }
--- puppet.d/sampleapp/manifests/bundle.pp
+++ puppet.d/sampleapp/manifests/bundle.pp
@@ -1,9 +1,9 @@
 class sampleapp::bundle {
   $app_path = '/var/www/sample_app'
   exec { 'sampleapp::bundle':
-    command => "bundle install",
-    user    => 'root',
-    group   => 'appuser',
+    command => "bundle install --path=vendor/bundle",
+    user    => 1000,
+    group   => 1000,
     creates => $app_path,
     cwd     => $app_path,
     path    => ['/bin', '/usr/bin'],
