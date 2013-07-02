c79f2fdf09e1c73ec31a6e08397c0f9f2ccce6cb
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-02 14:26:06 +0900

    [WIP]created unicorn init script

--- puppet.d/modules/monit/manifests/unicorn.pp
+++ puppet.d/modules/monit/manifests/unicorn.pp
@@ -1,10 +1,22 @@
 class monit::unicorn {
+  $unicorn_pid = '/var/www/sample_app/unicorn.pid'
+
+  file{ '/etc/init.d/unicorn':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    content => template('monit/unicorn.init.erb'),
+    notify  => Service['monit'],
+    require => Package['monit'],
+  }
+
   file{ '/etc/monit.d/unicorn':
     ensure  => present,
     owner   => root,
     group   => root,
     mode    => '0644',
-    content => template('monit/unicorn.erb'),
+    content => template('monit/unicorn.monit.erb'),
     notify  => Service['monit'],
     require => Package['monit'],
   }
