b902b285849951b1e6b7be50f0c2fd2a54f29d71
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-02 12:51:34 +0900

    add 'service enabled' to manifests

--- puppet.d/modules/monit/manifests/init.pp
+++ puppet.d/modules/monit/manifests/init.pp
@@ -2,8 +2,10 @@ class monit {
   include monit::install
   include monit::config
   include monit::unicorn
+  include monit::service
 
   Class['monit::install']
   -> Class['monit::config']
   -> Class['monit::unicorn']
+  -> Class['monit::service']
 }
