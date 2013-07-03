a579a3849a323ec6500c3a2aa7361d27081fd00d
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-03 19:13:20 +0900

    fix manifests for pubkeys

--- puppet.d/modules/common/manifests/pubkey.pp
+++ puppet.d/modules/common/manifests/pubkey.pp
@@ -1,9 +1,17 @@
 class common::pubkey {
+  file{ '/home/okkun/.ssh':
+    ensure  => directory,
+    owner   => 1000,
+    group   => 1000,
+    mode    => '0700'
+  }
+
   file{ '/home/okkun/.ssh/authorized_keys':
     ensure  => present,
     owner   => 1000,
     group   => 1000,
     mode    => '0600',
-    content => template('common/authorized_keys')
+    content => template('common/authorized_keys'),
+    require => File['/home/okkun/.ssh']
   }
 }
