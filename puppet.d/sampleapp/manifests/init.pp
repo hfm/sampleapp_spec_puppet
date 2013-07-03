93864bac4f035e7fecb81c3e8977781ead093d42
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-03 19:15:34 +0900

    leave sampleapp in change of capistrano

--- puppet.d/sampleapp/manifests/init.pp
+++ puppet.d/sampleapp/manifests/init.pp
@@ -1,9 +1,3 @@
 class sampleapp {
   include sampleapp::app_dir
-  include sampleapp::checkout
-  include sampleapp::bundle
-
-  Class['sampleapp::app_dir']
-  -> Class['sampleapp::checkout']
-  -> Class['sampleapp::bundle']
 }
