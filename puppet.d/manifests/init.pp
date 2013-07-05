0e403d3111dc468b2f0bf73d77c7590db66cb55d
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-05 15:08:45 +0900

    Fist, create user and groups

--- puppet.d/manifests/init.pp
+++ puppet.d/manifests/init.pp
@@ -1,4 +1,26 @@
-include user_group
+user { 'okkun':
+  ensure     => present,
+  comment    => 'okkun',
+  home       => '/home/okkun',
+  managehome => true,
+  shell      => '/bin/bash',
+  uid        => 1000,
+  gid        => 1000
+}
+
+group { 'appuser':
+  ensure => present,
+  gid    => 1000
+}
+
+file{ '/etc/sudoers':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0440',
+  content => template('sudoers')
+}
+
 include common
 include nginx
 include mysql
@@ -7,8 +29,7 @@ include ruby
 include sampleapp
 include monit
 
-Class['user_group']
--> Class['common']
+Class['common']
 -> Class['nginx']
 -> Class['mysql']
 -> Class['memcached']
