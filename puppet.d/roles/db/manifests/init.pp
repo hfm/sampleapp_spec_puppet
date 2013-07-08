36c1512903c899b18ffc9042de48e057de89641f
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-08 11:03:52 +0900

    DBのRoleを修正

--- puppet.d/roles/db/manifests/init.pp
+++ puppet.d/roles/db/manifests/init.pp
@@ -1,9 +1,3 @@
 class db {
-  include ::mysql
   include db::mysql
-
-     Class['::mysql::install']
-  -> Class['::mysql::config']
-  -> Class['::mysql::service']
-  -> Class['db::mysql::setup']
 }
