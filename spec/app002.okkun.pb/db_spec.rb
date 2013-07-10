eb9ab3c2b7a249d5c837f92d496bd6e439d8ce23
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-10 16:25:30 +0900

    app002用のspecを追加

---
+++ spec/app002.okkun.pb/db_spec.rb
@@ -0,0 +1,1 @@
+../app001.okkun.pb/db_spec.rb
---
+++ spec/app002.okkun.pb/memcached_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe host('app001.okkun.pb') do
+  it { should be_resolvable }
+  it { should be_reachable.with( :port => 11211, :proto => 'tcp' ) }
+end
